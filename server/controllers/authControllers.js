89fad49fa901fa100c3463d2d482b74423a09168
MAbdurahman <mdbdrrhm@gmail.com>
2021-06-16 07:34:43 -0500

    Modify signup method in authControllers; rename validator folder to validators

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,34 @@
-exports.signup = (req, res) => {
-    console.log('req.body on signup endpoint - ', req.body);
-    res.json({
-        data: 'This is signup endpoint from authControllers.js'
-    });
+const User = require('../models/userModel.js');
+
+exports.signup = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                error: 'Email is already register!',
+            });
+        }
+
+        let newUser = new User({ name, email, password });
+
+        await newUser.save((err, success) => {
+            if (err) {
+                console.log('Sign-up error', err);
+                return res.status(400).json({
+                    error: err,
+                });
+            }
+
+            res.json({
+                message: 'Sign-up success! Please sign-in!',
+            });
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: 'Email is already register!',
+        });
+    }
 };
